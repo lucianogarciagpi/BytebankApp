366ef84d3e3317cb06dada85d7c969a7c019d223
Luciano Garcia <43303397+lucianogarciagpi@users.noreply.github.com>
2022-06-15 07:28:15 -0300

    arquivo main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:bytebankapp/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(const BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.green[900]),
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: const ContactForm(),
+    );
+  }
+}
+
+
