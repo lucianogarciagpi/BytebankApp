8189b68b550943c4e0a88fc215eb5b730191883c
Luciano Garcia <43303397+lucianogarciagpi@users.noreply.github.com>
2022-08-19 10:20:52 -0300

    primeiro teste banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,41 @@
-import 'package:bytebankapp/screens/contact_form.dart';
+import 'package:bytebankapp/database/app_database.dart';
+import 'package:bytebankapp/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/dashboard.dart';
+import 'models/contact.dart';
 
 void main() {
   runApp(const BytebankApp());
+  save(Contact(0, 'alex', 1000)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
 }
 
 class BytebankApp extends StatelessWidget {
   const BytebankApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(color: Colors.green[900]),
-        primaryColor: Colors.green[900],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
-      home: const ContactForm(),
+          primaryColor: Colors.green[900],
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.green,
+          ).copyWith(
+            primary: Colors.green[900],
+            secondary: Colors.blueAccent[700],
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            primary: Colors.blueAccent[700],
+          )),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.green[900],
+            textTheme: ButtonTextTheme.primary,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.green[900])),
+      home: const Dashboard(),
     );
   }
 }
-
-
